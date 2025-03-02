455493fcf99d6f7fc067c685fb7ff5273d94c392
aspizu <aspizu@protonmail.com>
2025-03-02 19:56:42 +0530

    FEAT: add build script and package configuration for project setup

---
+++ build.ts
@@ -0,0 +1,21 @@
+import Packager from "@turbowarp/packager";
+import { $ } from "bun";
+import FS from "fs/promises";
+
+await $`
+python prebuild.py
+goboscript build
+`;
+
+const projectData = await FS.readFile("gsbounce.sb3");
+
+function progressCallback(type: any, a: any, b: any) {}
+
+const loadedProject = await Packager.loadProject(projectData, progressCallback);
+
+const packager = new Packager.Packager();
+packager.project = loadedProject;
+
+const result = await packager.package();
+
+await FS.writeFile(result.filename, result.data);
